b717e94b41b500cf27d086ca4ffcd461d1069fba
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-11-09 18:33:43 +0000

    Refactored work-section data to component level

--- src/app/work-section/work-section.component.ts
+++ src/app/work-section/work-section.component.ts
@@ -6,6 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work-section.component.css']
 })
 export class WorkSectionComponent implements OnInit {
+  title = "My Work";
+  subtitle = "RECENT WORK";
+
+  apps = [
+    {
+      name: "ChatBot",
+      domain: "Artificial Intelligence",
+      logo: "../assets/img/backend.jpeg",
+      likes: 0,
+      views: 0,
+      shares: 0
+    },
+{
+      name: "InfoSen",
+      domain: "Web Design",
+      logo: "../assets/img/frontend.jpeg",
+      likes: 0,
+      views: 0,
+      shares: 0
+    },
+{
+      name: "Kawtal",
+      domain: "Web Development",
+      logo: "../assets/img/frontbackend.png",
+      likes: 0,
+      views: 0,
+      shares: 0
+    }
+  ]
 
   constructor() { }
 
--- src/app/work-section/work/work.component.ts
+++ src/app/work-section/work/work.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-work',
@@ -6,6 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work.component.css']
 })
 export class WorkComponent implements OnInit {
+
+  @Input() 
+  name: string;
+
+  @Input()
+  domain: string;
+
+  @Input()
+  logo: string;
+
+  @Input()
+  views: number;
+
+  @Input()
+  likes: number;
+
+  @Input()
+  shares: number;
 
   constructor() { }
 
