0e8431aca616012d91fd62dfddf71e47a5e64721
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-11-11 15:59:34 +0000

    Refactored about section data

--- src/app/about-section/about-section.component.ts
+++ src/app/about-section/about-section.component.ts
@@ -1,21 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-about-section',
-  templateUrl: './about-section.component.html',
-  styleUrls: ['./about-section.component.css']
+  selector: "app-about-section",
+  templateUrl: "./about-section.component.html",
+  styleUrls: ["./about-section.component.css"]
 })
 export class AboutSectionComponent implements OnInit {
+  title = " About Us ";
+  subtitle = " Who I Am ? ";
+  text = `
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis doloremque ad incidunt sunt dicta facilis tempore neque minima! Nihil perferendis aut saepe amet? Reprehenderit quod ratione adipisci a dolore.
+  `;
+  topics = ["FrontEnd", "BackEnd", "AI", "Soft Eng"];
+  constructor() {}
 
-  topics = [
-    "FrontEnd",
-    "BackEnd",
-    "AI",
-    "Soft Eng"
-  ]
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
