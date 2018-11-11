9dc46819974adaba12d197e8618171f19974db2e
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-11-11 15:45:57 +0000

    Refactored menu data items to component level

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,16 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  selector: "app-menu",
+  templateUrl: "./menu.component.html",
+  styleUrls: ["./menu.component.css"]
 })
 export class MenuComponent implements OnInit {
   colors = ["lightblue", "lightpink"];
 
-  constructor() { }
+  items = [
+    {
+      name: "HOME",
+      link: ""
+    },
+    {
+      name: "ABOUT ME",
+      link: "about"
+    },
+    {
+      name: "MY SERVICES",
+      link: "services"
+    },
+    {
+      name: "MY SKILLS",
+      link: "skills"
+    },
+    {
+      name: "MY WORK",
+      link: "apps"
+    },
+    {
+      name: "CONTACT ME",
+      link: "contact"
+    }
+  ];
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
