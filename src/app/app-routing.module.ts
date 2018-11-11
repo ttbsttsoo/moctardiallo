62bde112d4759ed993dab4816f1124d8d23b1532
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-11-11 04:58:12 +0000

    Basic routing implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { SkillsSectionComponent } from "./skills-section/skills-section.component";
+import { AboutSectionComponent } from "./about-section/about-section.component";
+import { HomeSectionComponent } from "./home-section/home-section.component";
+import { AppComponent } from "./app.component";
+import { ServicesSectionComponent } from "./services-section/services-section.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { WorkSectionComponent } from "./work-section/work-section.component";
+import { ContactSectionComponent } from "./contact-section/contact-section.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeSectionComponent
+  },
+  {
+    path: "about",
+    component: AboutSectionComponent
+  },
+  {
+    path: "services",
+    component: ServicesSectionComponent
+  },
+  {
+    path: "skills",
+    component: SkillsSectionComponent
+  },
+  {
+    path: "work",
+    component: WorkSectionComponent
+  },
+  {
+    path: "contact",
+    component: ContactSectionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
